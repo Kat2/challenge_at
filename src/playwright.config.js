d7ef03ceb1293b7d076be54c44efb1b119ce521d
castelom <vitorcastelo.m@gmail.com>
2023-12-20 15:13:23 -0300

    Config trace

--- src/playwright.config.js
+++ src/playwright.config.js
@@ -24,11 +24,12 @@ module.exports = defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: false,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
