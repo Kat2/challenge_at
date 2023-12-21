9006c4a960d669f0726cafca1d50402a2afe2dfd
castelom <vitorcastelo.m@gmail.com>
2023-12-21 17:59:17 -0300

    Create withdraw test cases

--- src/page-objects/operation-page.js
+++ src/page-objects/operation-page.js
@@ -2,16 +2,24 @@ exports.OperationPage =
 class OperationPage {
     constructor(page){
         this.page = page;
+        this.withdrawErrorMsg = 'Transaction Failed. You can not withdraw amount more than the balance.';
+        this.depositSuccessMsg = 'Deposit Successful';
+        this.withdrawSuccessMsg = 'Transaction successful';
     }
 
-    async startOperation(operation, value){
-        await this.page.getByRole('button', { name: operation}).first().click();
+    async startDeposit(value){
+        await this.page.getByRole('button', { name: 'Deposit'}).first().click();
         await this.page.getByPlaceholder('amount').click();
         await this.page.getByPlaceholder('amount').fill(value.toString());
-        await this.page.getByRole('form').getByRole('button', { name: operation}).click();
+        await this.page.getByRole('form').getByRole('button', { name: 'Deposit'}).click();
     }
     
-    async startWithDrawl(value){
-
+    async startWithdraw(value){
+        //Withdraw button with wrong spelling, once corrected it will be possible to create a generic function for Deposit and Withdraw.
+        await this.page.getByRole('button', { name: 'Withdrawl'}).first().click();
+        await this.page.waitForTimeout(1000);
+        await this.page.getByPlaceholder('amount').click();
+        await this.page.getByPlaceholder('amount').fill(value.toString());
+        await this.page.getByRole('form').getByRole('button', { name: 'Withdraw'}).click();
     }
 }
--- src/tests/welcome-page.spec.js
+++ src/tests/welcome-page.spec.js
@@ -4,8 +4,11 @@ import { LoginPage } from '../page-objects/login-page';
 
 
 let page;
+let operationPage;
+let balance = 0;
 
 test.beforeAll('Login into the system', async({browser}) => {
+    //Need login to access Operations feature
     console.log("Login as Harry Potter");
     page = await browser.newPage();
     const login = new LoginPage(page);
@@ -20,60 +23,87 @@ test.beforeAll('Login into the system', async({browser}) => {
 });*/
 
 test.describe('Deposit Scenario', () => {
-    let operationPage;
-    let operation;
 
     test.beforeAll('Setup deposit test cases', async({})=> {
         operationPage = new OperationPage(page);
-        operation = 'Deposit'
     });
 
     test('Deposit a negative value in user account', async({}) => {
         const defaultDeposit = -100;
-        await operationPage.startOperation(operation, defaultDeposit)
-        await expect(operationPage.page.getByText('Deposit Successful', { exact: true })).toBeHidden();
+        await operationPage.startDeposit(defaultDeposit)
+        await expect(operationPage.page.getByText(operationPage.depositSuccessMsg, { exact: true })).toBeHidden();
+        await expect(operationPage.page.getByRole('strong').getByText(balance.toString(), { exact: true })).toBeVisible();
     });
 
     test('Deposit 0 USD in user account', async({}) => {
         const defaultDeposit = 0;
-        await operationPage.startOperation(operation, defaultDeposit)
-        await expect(operationPage.page.getByText('Deposit Successful', { exact: true })).toBeHidden();
+        await operationPage.startDeposit(defaultDeposit)
+        await expect(operationPage.page.getByText(operationPage.depositSuccessMsg, { exact: true })).toBeHidden();
+        await expect(operationPage.page.getByRole('strong').getByText(balance.toString(), { exact: true })).toBeVisible();
     });
 
     test('Deposit an invalid amount in user account', async({}) => {
         const defaultDeposit = 'joao';
-        await expect(operationPage.startOperation(operation, defaultDeposit)).rejects.toThrow();
-        await expect(operationPage.page.getByText('Deposit Successful', { exact: true })).toBeHidden();
+        await expect(operationPage.startDeposit(defaultDeposit)).rejects.toThrow();
+        await expect(operationPage.page.getByText(operationPage.depositSuccessMsg, { exact: true })).toBeHidden();
+        await expect(operationPage.page.getByRole('strong').getByText(balance.toString(), { exact: true })).toBeVisible();
     });
 
     test('Deposit a positive value in user account', async({}) => {
-        const defaultDeposit = 900;
-        await operationPage.startOperation(operation, defaultDeposit)
-        await expect(operationPage.page.getByText('Deposit Successful', { exact: true })).toBeVisible();
-        await expect(operationPage.page.getByRole('strong').getByText(defaultDeposit.toString())).toBeVisible();
+        const defaultDeposit = 1000;
+        await operationPage.startDeposit(defaultDeposit);
+        balance += defaultDeposit;
+        await expect(operationPage.page.getByText(operationPage.depositSuccessMsg, { exact: true })).toBeVisible();
+        await expect(operationPage.page.getByRole('strong').getByText(balance.toString(), { exact: true })).toBeVisible();
     });
 });
-/*
+
 test.describe('Withdrawl Scenarios', () =>{
-    let operationPage;
+    
 
-    test.beforeAll('Setup deposit test cases', async({})=> {
+    test.beforeAll('Setup withdraw tests', async({})=> {
         operationPage = new OperationPage(page);
+        //const deposit = 1000;
+        //await operationPage.startDeposit(deposit);
     });
 
-    test.afterEach('Reload Page', async({})=> {
-        operationPage.page.reload();
-    })
-
-    test('Withdrawl a value >= the balance', async({}) => {
-        const defaultWithdrwal = 100;
-        await operationPage.startDeposit(defaultDeposit)
-        await expect(operationPage.page.getByText('Deposit Successful', { exact: true })).toBeVisible();
-        await expect(operationPage.page.getByText(operationPage.balance.toString(), { exact: true })).toBeVisible();
+    test('Withdraw a value >= the balance', async({}) => {
+        const defaultWithdraw = 1001;
+        await operationPage.startWithdraw(defaultWithdraw);
+        await expect(operationPage.page.getByText(operationPage.withdrawErrorMsg, { exact: true })).toBeVisible();
+        await expect(operationPage.page.getByRole('strong').getByText(balance.toString(), { exact: true })).toBeVisible();
     });
-})*/
 
-test('Loged user', async({}) =>{
+    test('Withdraw a value invalid', async({}) => {
+        const defaultWithdraw = 'Joao';
+        await expect(operationPage.startWithdraw(defaultWithdraw)).rejects.toThrow();
+        await expect(operationPage.page.getByText(operationPage.withdrawErrorMsg, { exact: true })).toBeVisible();
+        await expect(operationPage.page.getByRole('strong').getByText(balance.toString(), { exact: true })).toBeVisible();
+    });
+
+    test('Withdraw a value <= the balance', async({}) => {
+        const defaultWithdraw = 1000;
+        await operationPage.startWithdraw(defaultWithdraw);
+        balance -= defaultWithdraw;
+        await expect(operationPage.page.getByText(operationPage.withdrawSuccessMsg, { exact: true })).toBeVisible();
+        await expect(operationPage.page.getByRole('strong').getByText(balance.toString(), { exact: true })).toBeVisible();
+    });
+});
+
+test.describe('Load screen test', ()=> {
+    test.beforeAll('Reload page', async({})=> {
+        const defaultDeposit = 100;
+        await operationPage.startDeposit(defaultDeposit);
+        balance += defaultDeposit;
+        await operationPage.page.reload();
+    });
+
+    test('Page reload test', async({})=>{
+        await expect(operationPage.page.getByRole('strong').getByText(balance.toString(), { exact: true })).toBeVisible();
+    });
+});
+
+/*test('Loged user', async({}) =>{
     //Expect a title
     await expect(page.getByText('Welcome Harry Potter !!', { exact: true })).toBeVisible();
-})
+})*/
